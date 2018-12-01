19535db845d48ea9ee69be1068c64dbc03ea86ac
jingyilin1995 <liuxuelinjingyi@163.com>
2018-12-01 02:19:57 -0800

    Create SNSClient and Create SNSTopic

---
+++ Assignment3/eclipse-cloudcourse/coursepractise/src/main/java/com/csye6225/fall2018/coursepractise/datamodel/DynamoDBTester.java
@@ -0,0 +1,39 @@
+package com.csye6225.fall2018.coursepractise.datamodel;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.GetItemRequest;
+import com.amazonaws.services.dynamodbv2.model.GetItemResult;
+
+public class DynamoDBTester {
+	static AmazonDynamoDB dynamoDB;
+	private static void init() throws Exception{
+		AWSCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
+		credentialsProvider.getCredentials();
+		
+		dynamoDB = AmazonDynamoDBClientBuilder
+				.standard()
+				.withCredentials(credentialsProvider)
+				.withRegion("us-east-2")
+				.build();
+	}
+	public static void main(String[] args) throws Exception{
+		init();
+		String tableName = "students-test";
+		
+		GetItemRequest getItemRequest = new GetItemRequest();
+		Map<String, AttributeValue> itemToFetch = new HashMap<>();
+		itemToFetch.put("studentId", new AttributeValue().withS("123"));
+		getItemRequest.setKey(itemToFetch);
+		getItemRequest.setTableName(tableName);
+		GetItemResult getItemResult = dynamoDB.getItem(getItemRequest);
+		System.out.println("GetItemResult:" + getItemResult);
+	}
+
+}
